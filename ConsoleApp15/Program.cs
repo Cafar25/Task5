0199dc9320a680185f46188c41a08af843fdcef0
jafarka <jafarka@CATR5>
2023-03-18 11:51:24 +0400

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,491 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            // int eded = 0;
+            // int eded1 = 0;
+            // for (int i = 0; i <= 1000; i++)
+            // {
+            //     eded = i;
+            //     eded1 += i;
+            // }
+            // if (eded1 % 7 == 0 )  
+            // {
+
+            //     Console.WriteLine($"bolunur {eded1} / 7 = {eded1/7}");
+            // }
+            //else
+            // {
+            //     Console.WriteLine("Bolunmur");
+            // }
+            #endregion
+            #region task2
+            //int eded = 0;
+
+            //for (int i = 0; i <= 1000; i++)
+            //{
+            //    eded = i;
+
+            //    if (eded % 7 == 0 && eded % 8 == 0 && eded > 0)
+            //    {
+            //        Console.WriteLine(eded);
+            //    }
+            //}
+            #endregion
+            #region task3
+            //int eded = 0;
+            //int eded1 = 0;
+
+            //for(int i = 0;i<1000;i++) 
+            //{
+            //    eded = i;
+            //    while(eded>0) 
+            //    {
+            //        int eded2 = eded % 10;
+            //        eded /= 10;
+            //        eded1 += eded2;
+            //    }
+            //    if (eded1 % 3 == 0 && eded1 > 0)  
+            //    {
+            //        Console.WriteLine(eded1);
+            //    }
+
+            //}
+            #endregion
+            #region task4
+            //int eded = 0;
+            //int eded1 = 0;
+            //int eded3= 0;
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    eded = i;
+            //    while (eded > 0)
+            //    {
+            //        int eded2 = eded % 10;
+            //        eded /= 10;
+            //        eded1 += eded2;
+            //        eded3 = eded1 % 10;
+            //    }
+            //    if(eded3%10==3) 
+            //    {
+            //        continue;
+            //    }
+            //    if (eded1 % 3 == 0 && eded1 > 0)
+            //    {
+            //        Console.WriteLine(eded1);
+            //    }
+
+            //}
+            #endregion
+            #region task5
+
+            //int eded = 0;
+            //int eded1 = 0;
+            //int eded3 = 0;
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    eded = i;
+            //    while (eded > 0)
+            //    {
+            //        int eded2 = eded % 10;
+            //        eded /= 10;
+            //        eded1 += eded2;
+            //        eded3 = eded1 % 10;
+            //    }
+
+            //    if (eded1 % 5 == 0 && eded1 > 0 && eded % 5 == 0)
+            //    {
+            //        Console.WriteLine(eded1);
+            //    }
+
+            //}
+            #endregion
+            #region task6
+            //int eded = 0;
+            //int eded1 = 0;
+            //int eded2 = 0;
+            //int eded3 = 0;
+            //int eded4 = 0;
+            //for (int i = 0; i < 1000; i++)
+            //{
+
+            //    eded = i;
+            //    eded4 = eded;
+            //    while (eded > 0)
+            //    {
+
+            //        eded2 = eded % 10;
+            //        eded /= 10;
+            //        eded1 += eded2;
+
+            //    }
+            //    if (eded4 % 2 == 0 && eded1 % 2 == 1 && eded2 % 2 == 1)
+            //    {
+            //        Console.WriteLine($"eded:{eded4}");
+            //    }
+
+            //}
+
+            #endregion
+            #region task7
+            //int eded = 0;
+            //int eded1 = 0;
+            //int eded2 = 0;
+
+            //for (int i = 0; i < 1000; i++) 
+            //{
+            //    eded = i;
+            //    eded1 = eded;
+
+            //    eded2 = eded % 10;
+            //    eded /= 10;
+
+
+            //        if (!(eded2 == 3))
+            //        {
+            //            Console.WriteLine(eded1);
+            //        }
+
+            //}
+            #endregion
+            #region task8
+            //int eded = 0;
+            //int eded1 = 0;
+            //int eded2 = 0;
+            //int eded3 = 0;
+            //int eded4 = 0;
+            //int eded5 = 0;
+            //int eded6 = 0;
+            //int eded7 = 0;
+            //for (int i = 0; i <= 1000; i++)
+            //{
+            //    eded = i;
+            //    eded3 = i;
+            //    eded1 = eded;
+            //    eded2 = eded % 10;
+            //    eded /= 10;
+
+            //    eded4 = eded3 % 10;
+            //    eded5 = eded3 % 100 / 10;
+            //    eded6 = eded3 % 1000 / 100;
+            //    eded7 = eded3 % 10000 / 1000;
+            //    eded7 += eded4 + eded5 + eded6;
+
+            //    if (eded2 != 3 && eded7 == 3)
+            //    {
+            //        Console.WriteLine(eded1);
+            //    }
+
+            //}
+            #endregion
+            #region task9
+            //int eded = 0 ;
+            //int eded1 = 0;
+            //int eded2 = 0;
+            //int eded3 = 0;
+            //int eded4 = 0;
+            //int eded5 = 0;
+
+            //for (int i = 0; i <= 1000; i++) 
+            //{
+            //    eded = i;
+
+            //    eded1 = eded % 10;
+            //    eded2 = eded % 100 / 10;
+            //    eded3 = eded % 1000 / 100;
+            //    eded4 = eded % 10000 / 1000;
+
+            //    eded5 = eded1 + eded2 + eded3 + eded;
+
+            //    if (eded5 % 11 == 0 && eded5 > 11)
+            //    {
+            //        Console.WriteLine(eded);
+            //    }
+            //}
+            #endregion
+            #region task10
+            //int eded = 0;
+            //int eded01 = 0;
+            //int eded1 = 0;
+            //int eded2 = 0;
+            //int eded3 = 0;
+
+            //int eded4 = 0;
+            //int eded5 = 0;
+            //int eded6 = 0;
+            //int eded7 = 0;
+            //for (int i = 0; i <= 1000; i++) 
+            //{
+            //    eded = i;
+            //    eded01 = i;
+            //    if(eded % 2 == 0)
+            //    {
+
+            //        eded1 = eded % 10;
+            //        eded2 = eded % 100 / 10;
+            //        eded3 = eded % 1000 / 100;
+            //        eded4= eded % 10000 / 1000;
+
+            //        eded5 = eded1 + eded2 + eded3 + eded4;
+            //        eded6 = eded5;
+            //        if (eded5 > 5 && eded5 < 7 && eded6 > eded5)
+            //        {
+            //            Console.WriteLine(eded01);
+            //        }
+            //    }
+            //}
+            #endregion
+            #region task11
+            #region usul1
+            //int eded = 0;
+            //int eded1 = 0;
+            //int eded2 = 0;
+            //int eded3 = 0;
+            //int eded4 = 0;
+            //int eded5 = 0;
+            //int eded6 = 0;
+            //int eded7 = 0;
+            //int newnum;
+            //int a;
+            //for (int i = 0; i <= 1000; i++)
+            //{
+            //    newnum = i;
+            //    eded = i;
+            //    eded3 = i;
+            //    eded1 = eded;
+            //    eded2 = eded % 10;
+            //    eded /= 10;
+
+            //    eded4 = eded3 % 10;
+            //    eded5 = eded3 % 100 / 10;
+            //    eded6 = eded3 % 1000 / 100;
+            //    eded7 = eded3 % 10000 / 1000;
+
+            //    eded7 += eded4 + eded5 + eded6;
+            //    Console.WriteLine(eded);
+            //    if (eded2 != 3 && eded7 > 10 && eded3 == eded8)
+            //    {
+            //        Console.WriteLine(eded1);
+            //    }
+
+            //}
+            #endregion
+            int qaliq = 0;
+            int a = 0;
+            int newnum = 0;
+            int newnum1 = 0;
+            int qaliq1 = 0;
+
+       
+            for (int i = 1; i <= 1000; i++)
+            {
+               l1:
+                newnum = 0;
+                a = i;
+                int b = a;
+                int c = a;
+
+                while (c > 0)
+                {
+                    qaliq1 = c % 10;
+                    c /= 10;
+                    newnum1 = newnum1 + qaliq1;
+                }
+
+
+                while (a > 0)
+                {
+                    qaliq = a % 10;
+                    a /= 10;
+                    
+                    if (qaliq == 3)
+                    {
+                        i++;
+                        goto l1;
+                    }
+                    newnum = newnum * 10 + qaliq;
+                }
+
+                if (newnum == i && newnum1 > 10 && b > 10)
+                {
+                    Console.WriteLine(b);
+                }
+            }
+            #endregion
+            #region task12
+            //int eded = 0;
+            //int qaliq = 0;
+            //int eded1 = 0;
+            //int qaliq1 = 0;
+            //int eded2 = 0;
+            //int qaliq2 = 0;
+            //int eded3 = 0;
+            //for(int i = 0;i <= 1000;i++) 
+            //{
+            //    eded = i;
+            //    eded2 = i;
+            //    while (eded > 0)
+            //    {
+            //        qaliq = eded % 10;
+            //        eded /= 10;
+            //        eded1 = eded1 + qaliq;
+            //    }
+            //    while (eded2 > 0)
+            //    {
+            //        qaliq2 = eded2 % 10;
+            //        eded2 /= 10;
+            //        eded3 = eded3 + qaliq2;
+            //    }
+            //    int a = i % 10;
+            //    int b = i % 100 / 10;
+            //    int c = i % 1000 / 100;
+            //    if (a == b && b == c && i > 0 && qaliq2 != 5 && eded1 > 5)
+            //    {
+            //        Console.WriteLine(i);
+            //    } 
+            //}
+            #endregion
+            #region task13
+            //int qaliq = 0;
+            //int a = 0;
+            //int newnum = 0;
+            //int newnum1 = 0;
+            //int qaliq1 = 0;
+
+            //for (int i = 1; i <= 100000; i++)
+            //{
+            //    newnum = 0;
+            //    a = i;
+            //    int b = a;
+            //    int c = a;
+            //    int t = 0;
+
+            //    while (a > 0)
+            //    {
+            //        qaliq = a % 10;
+            //        a /= 10;
+            //        newnum = (newnum * 10) + qaliq;
+            //        if(qaliq == 5)
+            //        {
+            //            t++;
+            //        }
+            //    }
+            //    int a1 = i % 10;
+            //    int b1 = i % 100 / 10;
+            //    int c1 = i % 1000 / 100;
+            //    int d1 = i % 10000 / 1000;
+            //    int e1 = i % 100000 / 10000;
+            //    int f1 = i % 1000000 / 100000;
+            //    if (newnum == i && qaliq % 2 == 1 && (a1 == 5 || b1 == 5 || c1 == 5 || d1 == 5 || e1 == 5 || f1 == 5) && (t < 2) && i > 10)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region task14
+            //int eded = 0;
+            //int qaliq = 0;
+            //int eded01 = 0;
+            //int eded02 = 0;
+            //int sum = 0;
+            //int eded2 = 0;
+            //for (int i = 0; i <= 100000; i++) 
+            //{
+            //    qaliq = i % 10;
+            //    eded = i;
+            //    while(eded > 0)
+            //    {
+            //        if(eded % 10 == 6 )
+            //        {
+            //            break;
+            //        }
+            //        if(eded % 100 == 11)
+            //        {
+            //            eded02 = 2;
+            //        }
+            //        if(eded % 10 == 5)
+            //        {
+            //            eded01++;
+            //        }
+            //        eded /= 10;
+            //    }
+            //    if(eded02 == 2 && eded01 == 2)
+            //    {
+            //        eded2 = i;
+            //        while(eded02 > 0)
+            //        {
+            //            sum += eded2 % 10;
+            //            eded2 /= 10;
+            //        }
+            //        if(sum > 7 && sum < 15)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //}
+            #endregion
+            #region task15
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    int sum = 0;
+            //    int demo = i;
+            //    while (demo != 0)
+            //    {
+            //        sum += demo % 10;
+            //        demo /= 10;
+            //    }
+            //    bool cont = false;
+            //    int num = sum;
+            //    while (num > 0)
+            //    {
+            //        int digit = num % 10;
+            //        if (digit == 2)
+            //        {
+            //            cont = true;
+            //            break;
+            //        }
+            //        num /= 10;
+            //    }
+            //    if (!cont)
+            //    {
+            //        bool cont1 = false;
+            //        int num1 = i;
+            //        while (num1 > 0)
+            //        {
+            //            int digit = num1 % 10;
+            //            if (digit == 0 || digit == 1 || digit == 5 || digit == 7 || digit == 9)
+            //            {
+            //                cont1 = true;
+            //                break;
+            //            }
+            //            num1 /= 10;
+            //        }
+            //        if (!cont1)
+            //        {
+            //            bool cont2 = false;
+            //            int number = i;
+            //            int half = number / 2;
+            //            for (int j = 2; j <= half; j += 2)
+            //            {
+            //                if (number % j == 0)
+            //                {
+            //                    cont2 = true;
+            //                    break;
+            //                }
+            //            }
+            //            if (!cont2)
+            //            {
+            //                Console.WriteLine(i);
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+        }
+    }
+}
